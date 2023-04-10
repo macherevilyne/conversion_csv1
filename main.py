90289d16a637a13381eda0bcb601b5716c87eacb
Aliaksandr Lapunou <109434373+macherevilyne@users.noreply.github.com>
2023-04-10 16:18:19 +0300

    Changing the comment language and creating a configuration file

--- main.py
+++ main.py
@@ -2,58 +2,67 @@
 import os
 import pandas as pd
 import mysql.connector
+import configparser
 
-"""CHANGING db_name TO THE CURRENT DATA TO CONNECT TO YOUR DATABASE"""
-# Название базы данных, к которой вы хотите подключиться
-db_name = "csv"
 
-"""CHANGING HOST, PORT, USER, PASSWORD TO THE CURRENT DATA TO CONNECT TO YOUR DATABASE"""
-# Установка соединения с базой данных MySQL
-mydb = mysql.connector.connect(
-    host="localhost",
-    port=3306,
-    user="admin",
-    password="11111111Qwe`")
 
-# Получение списка существующих баз данных
-cursor = mydb.cursor()
+# Reading connection parameters from the configuration file
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+
+
+# Database Connection
+cnx = mysql.connector.connect(
+    host=config['mysql']['host'],
+    user=config['mysql']['user'],
+    password=config['mysql']['password'],
+
+)
+
+
+
+# Getting a List of Existing Databases
+cursor = cnx.cursor()
 cursor.execute("SHOW DATABASES")
 databases = cursor.fetchall()
-# Список имен существующих баз данных
+
+
+# List of existing database names
 existing_databases = [database[0] for database in databases]
 
+# Database name
+db_name = config['mysql']['database']
 
-"""CHANGING HOST, PORT, USER, PASSWORD TO THE CURRENT DATA TO CONNECT TO YOUR DATABASE"""
 
-# Если нужная база данных уже существует, подключаемся к ней
+# If the required database already exists, we connect to it
 if db_name in existing_databases:
-    mydb = mysql.connector.connect(
-        host="localhost",
-        port=3306,
-        user="admin",
-        password="11111111Qwe`",
-        database=db_name
+    cnx = mysql.connector.connect(
+        host=config['mysql']['host'],
+        user=config['mysql']['user'],
+        password=config['mysql']['password'],
+        database=config['mysql']['database']
     )
     print('База данных {}'.format(db_name),
           "уже существует.Подключение к базе данных {} успешно выполнено".format(db_name))
 
-# Если нужная база данных не существует, создаем ее и подключаемся к ней
+# If the required database does not exist, create it and connect to it
 else:
     cursor.execute("CREATE DATABASE {}".format(db_name))
-    mydb = mysql.connector.connect(
-        host="localhost",
-        port=3306,
-        user="admin",
-        password="11111111Qwe`",
-        database=db_name
+    cnx = mysql.connector.connect(
+        host=config['mysql']['host'],
+        user=config['mysql']['user'],
+        password=config['mysql']['password'],
+        database=config['mysql']['database']
     )
+    print("Database {} was successfully created and connected to".format(db_name))
 
-    print("Баз данных {} успешно создана и к ней выполнено подключение ".format(db_name))
 
-# Создание таблицы в базе данных MySQL
-"""CHANGING THE TABLE TO THE CURRENT DATA TO CONNECT TO YOUR DATABASE"""
 
-cursor = mydb.cursor()
+# Creating a Table in a MySQL Database
+"""CHANGING THE TABLE 22q2 TO THE CURRENT DATA TO CONNECT TO YOUR DATABASE"""
+
+cursor = cnx.cursor()
 cursor.execute(
     "CREATE TABLE IF NOT EXISTS 22q2 (Product_Group VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,"
     "Product VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,"
@@ -89,31 +98,30 @@ cursor.execute(
     "Combined_Ratio VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,"
     "UNIQUE KEY unique_policy_product (Product,Policy_Number, Subproduct))")
 
-#  Закрытие курсора и сохранение изменений в базе данных
-cursor.close()
-mydb.commit()
+#  Save changes to the database
 
-# Создание курсора
-cursor = mydb.cursor()
+cnx.commit()
 
-"""YOU NEED TO CHANGE THE DIRECTORY VARIABLE TO THE CURRENT DIRECTORY (PATH) WHERE THE CSV FILES WILL BE READ FROM """
 
-# Получение списка файлов CSV из заданной директории
-directory = r'h:\IT\First_project'
+
+"""CHANGING NAME TABLE:22q2 in INSERT INTO sql TO THE CURRENT DATA TO CONNECT TO YOUR DATABASE"""
+
+# Retrieving a list of CSV files from a specified directory
+directory = config['mysql']['directory']
 files = os.listdir(directory)
 csv_files = [f for f in files if f.endswith('.csv')]
 
-# Чтение файлов CSV и запись данных в базу данных MySQL
+# Read CSV files and write data to MySQL database
 for file in csv_files:
     file_path = os.path.join(directory, file)
     df = pd.read_csv(file_path, low_memory=False)
     df = df.where(pd.notnull(df), '')
-    print(f"ВСЕГО ЗАПИСЕЙ НУЖНО СДЕЛАТЬ ИЗ DATAFRAME В БАЗУ ДАННЫХ:", len(df))
+    print(f"TOTAL RECORDS TO BE MADE FROM DATAFRAME TO DATABASE:", len(df))
 
     records_inserted = 0
     try:
         for index, row in df.iterrows():
-            cursor = mydb.cursor()
+            cursor = cnx.cursor()
             sql = "INSERT INTO 22q2 (Product_Group, Product, Subproduct, Policy_Number, Status, Gender, Age, Joint_Life," \
                   " Start_Date, Lapse_Date, End_Date, Annual_Premium, Single_Premium, Premium_Indexation, Changing_Premium, " \
                   "Fonds_Value, PVPrem, PVFP, PVCost, PVSCR, Statutory_Res, SII_Res, Death_Benefit_Sum, Sum_of_Salary, " \
@@ -125,12 +133,12 @@ for file in csv_files:
             records_inserted += cursor.rowcount
     except mysql.connector.IntegrityError as error:
         if error.errno == 1062:  # код ошибки дубликата ключа
-            print(f'ПРИ ЗАГРУЗКЕ ЕСТЬ ДУБЛИКАТЫ ЗАПИСЕЙ, ДУБЛИКАТЫ НЕ БУДУТ ДОБАВЛЕНЫ')
+            print(f'THERE ARE DUPLICATE RECORDS ON DOWNLOAD, NO DUPLICATES WILL BE ADDED')
         else:
-            print(f"Ошибка: {error}")
-    print(f"ВСЕГО ЗАПИСЕЙ СДЕЛАНО В БАЗУ ДАННЫХ: {records_inserted}")
+            print(f"Error: {error}")
+    print(f"TOTAL RECORDS MADE TO DATABASE: {records_inserted}")
 
-# Закрытие курсора и базы данных
-mydb.commit()
+# Close the cursor and save the database
+cnx.commit()
 cursor.close()
-mydb.close()
+cnx.close()
